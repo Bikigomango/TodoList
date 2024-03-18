bbae4e474a9275f467304295a19ef12ec4346885
Biki Gamang <129982236+Bikigomango@users.noreply.github.com>
2024-03-18 11:53:19 +0530

    Supabase.js

--- mypro/src/Components/Supabase.js
+++ mypro/src/Components/Supabase.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 8000;
 const supabase = createClient(
   "https://axecuodyqkzojnxmhcvy.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4ZWN1b2R5cWt6b2pueG1oY3Z5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkyNjAwNjMsImV4cCI6MjAyNDgzNjA2M30.d685EZYleyYOVv7y4Ts7LbQW64im8F0f9rVhSBRM4Dk"
+  "api__key"
 );
 
 app.use(express.json());
